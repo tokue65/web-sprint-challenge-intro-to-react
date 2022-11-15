c499ac032eb4363b99b93ee41ba9ff4b01798995
Kelsie Honda <kelsiehonda@Kelsies-MacBook-Pro.local>
2022-11-15 11:21:22 -1000

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./App.css";
+import { handlers, data } from './mocks/handlers';
+import Character from './components/Character';
 
-const App = () => {
+
+function App(props) {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,6 +16,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character/>
+      {/*MAPPING OVER DATA*/}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react"
+import {data} from "../mocks/handlers"
+import axios from 'axios';
+
+function Character({ props }) {
+    const [starWars, setStarWars] = useState ([]);
+
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/`)
+        .then(res => {
+        //const element- document.querySelector()
+            console.log(res.data);
+            setStarWars(res.data);
+        }).catch(err => console.error(err))
+    }, [])
+
+    return (
+        <div className='character container'>
+            {starWars.map(char => 
+                <div className="character" key={char.name}>
+                    {char.name} Birth Year: {char.birth_year}
+                </div>
+                    )}
+        </div>
+        )
+    }
+
+export default Character; 
